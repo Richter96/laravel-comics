512f79c6ed7179256570b698c0c8bf01116960dc
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-23 16:56:16 +0200

    add page comix

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/comix', function () {
+    return view('comix');
 });

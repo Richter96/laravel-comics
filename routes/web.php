ae0ebbe6a222c240bff2c9475f3e7c7ef4b82853
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-23 17:14:27 +0200

    add img and header active

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 Route::get('/comix', function () {
     return view('comix');
-});
+})->name('comix');

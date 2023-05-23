b52615d2a36b9cdbffd9c6e1c54ee0f0e4b89a02
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-23 16:56:07 +0200

    add partials

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header id="app_header">
+    <div class="container-md d-flex align-items-center justify-content-between">
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <nav class="nav justify-content-center">
+            <a class="nav-link active" href="/" aria-current="page">CHARACTERS</a>
+            <a class="nav-link " href="/comix" aria-current="page">COMICS</a>
+            <a class="nav-link " href="#" aria-current="page">MOVIES</a>
+            <a class="nav-link " href="#" aria-current="page">TV</a>
+            <a class="nav-link " href="#" aria-current="page">GAMES</a>
+            <a class="nav-link " href="#" aria-current="page">COLLECTIBLES</a>
+            <a class="nav-link " href="#" aria-current="page">VIDEOS</a>
+            <a class="nav-link " href="#" aria-current="page">FANS</a>
+            <a class="nav-link " href="#" aria-current="page">NEWS</a>
+            <a class="nav-link " href="#" aria-current="page">SHOP</a>
+        </nav>
+    </div>
+</header>

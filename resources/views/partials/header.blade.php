458b0d294a5e8bf359303311a3d600d9bc6d292f
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-23 17:30:59 +0200

    add active class

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,16 +4,31 @@
             <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="">
         </div>
         <nav class="nav justify-content-center">
-            <a class="nav-link text-black {{Route::currentRouteName()=== 'home' ? 'bg-primary text-white' : ''}} " href="{{route('home')}}" aria-current="page">CHARACTERS</a>
-            <a class="nav-link text-black {{Route::currentRouteName()=== 'comix' ? 'bg-primary text-white' : ''}}" href="{{route('comix')}}" aria-current="page">COMICS</a>
-            <a class="nav-link text-black {{Route::currentRouteName()=== '#' ? 'bg-primary text-white' : ''}}" href="#" aria-current="page">MOVIES</a>
-            <a class="nav-link text-black {{Route::currentRouteName()=== '#' ? 'bg-primary text-white' : ''}}" href="#" aria-current="page">TV</a>
-            <a class="nav-link text-black {{Route::currentRouteName()=== '#' ? 'bg-primary text-white' : ''}}" href="#" aria-current="page">GAMES</a>
-            <a class="nav-link text-black {{Route::currentRouteName()=== '#' ? 'bg-primary text-white' : ''}}" href="#" aria-current="page">COLLECTIBLES</a>
-            <a class="nav-link text-black {{Route::currentRouteName()=== '#' ? 'bg-primary text-white' : ''}}" href="#" aria-current="page">VIDEOS</a>
-            <a class="nav-link text-black {{Route::currentRouteName()=== '#' ? 'bg-primary text-white' : ''}}" href="#" aria-current="page">FANS</a>
-            <a class="nav-link text-black {{Route::currentRouteName()=== '#' ? 'bg-primary text-white' : ''}}" href="#" aria-current="page">NEWS</a>
-            <a class="nav-link text-black {{Route::currentRouteName()=== '#' ? 'bg-primary text-white' : ''}}" href="#" aria-current="page">SHOP</a>
+            <a class="nav-link {{Route::currentRouteName()=== 'home' ? 'active' : ''}} " href="{{route('home')}}" aria-current="page">CHARACTERS</a>
+            <a class="nav-link {{Route::currentRouteName()=== 'comix' ? 'active' : ''}}" href="{{route('comix')}}" aria-current="page">COMICS</a>
+            <a class="nav-link {{Route::currentRouteName()=== '#' ? 'active' : ''}}" href="#" aria-current="page">MOVIES</a>
+            <a class="nav-link {{Route::currentRouteName()=== '#' ? 'active' : ''}}" href="#" aria-current="page">TV</a>
+            <a class="nav-link {{Route::currentRouteName()=== '#' ? 'active' : ''}}" href="#" aria-current="page">GAMES</a>
+            <a class="nav-link {{Route::currentRouteName()=== '#' ? 'active' : ''}}" href="#" aria-current="page">COLLECTIBLES</a>
+            <a class="nav-link {{Route::currentRouteName()=== '#' ? 'active' : ''}}" href="#" aria-current="page">VIDEOS</a>
+            <a class="nav-link {{Route::currentRouteName()=== '#' ? 'active' : ''}}" href="#" aria-current="page">FANS</a>
+            <a class="nav-link {{Route::currentRouteName()=== '#' ? 'active' : ''}}" href="#" aria-current="page">NEWS</a>
+            <a class="nav-link {{Route::currentRouteName()=== '#' ? 'active' : ''}}" href="#" aria-current="page">SHOP</a>
         </nav>
     </div>
 </header>
+
+<style>
+    nav a {
+        color: #0c7cec;
+        text-decoration: none;
+        padding: 0.5rem 1rem;
+        border-bottom: 3px solid #0c7cec;
+    }
+
+    nav a:not(.active) {
+        border-bottom: none;
+        color: black;
+
+    }
+</style>
